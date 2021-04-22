6fc25c2e5ccfd70c538fc6ceba545d609a711d71
AgathonSF <82924142+AgathonSF@users.noreply.github.com>
2021-04-22 11:15:04 +0700

    Add files via upload

---
+++ halo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+print("Halo Dunia!")
+
+
+# In[22]:
+
+
+print ('|---------------------------------|')
+print ('|                                 |')
+print ('|            halo.py              |')
+print ('| agathonariel223@mail.ugm.ac.id  |')
+print ('|                                 |')
+print ('|---------------------------------|')
+
+
+# In[23]:
+
+
+print("Sekian dan Terima Kasih")
+
+
+# In[ ]:
+
+
+
+
